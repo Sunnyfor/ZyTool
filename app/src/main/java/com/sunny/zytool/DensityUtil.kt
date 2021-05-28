f7dd5246698d6a3fc8302914b4f3c4b6dc93087a
chenyongzuo <yongzuo.chen@foxmail.com>
2021-05-28 15:03:50 +0800

    首次提交

---
+++ app/src/main/java/com/sunny/zytool/DensityUtil.kt
@@ -0,0 +1,315 @@
+package com.sunny.zytool
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
+import android.util.DisplayMetrics
+import android.util.Log
+import android.util.TypedValue
+import android.view.View
+import android.view.ViewConfiguration
+import android.view.WindowManager
+
+/**
+ * Desc 屏幕密度工具类
+ * Author ZY
+ * Mail zuoyu98@foxmail.com
+ * Date 2019/10/25 11:03
+ */
+object DensityUtil {
+
+    /**
+     * 通过反射，获取包含虚拟键的整体屏幕高度
+     */
+    fun getHasVirtualKey(context: Context): Int {
+        var dpi = 0
+        val windowManager =
+            context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val dm = DisplayMetrics()
+        val c: Class<*>
+        try {
+            c = Class.forName("android.view.Display")
+            val method =
+                c.getMethod("getRealMetrics", DisplayMetrics::class.java)
+            method.invoke(display, dm)
+            dpi = dm.heightPixels
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return dpi
+    }
+
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    fun dip2px(context: Context, dpValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (dpValue * scale + 0.5f).toInt()
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    fun px2dip(context: Context, pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
+    }
+
+    fun dp2pxConvertInt(context: Context, dpValue: Float): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dpValue,
+            context.resources.displayMetrics
+        ).toInt()
+    }
+
+    fun sp2pxConvertInt(context: Context, spValue: Float): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            spValue,
+            context.resources.displayMetrics
+        ).toInt()
+    }
+
+    fun dp2px(context: Context, dpValue: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dpValue,
+            context.resources.displayMetrics
+        )
+    }
+
+    fun sp2px(context: Context, spValue: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            spValue,
+            context.resources.displayMetrics
+        )
+    }
+
+    /**
+     * 获取手机屏幕的高度
+     */
+    fun screenHeight(context: Context): Int {
+        return context.resources.displayMetrics.heightPixels
+    }
+
+    /**
+     * 获取手机屏幕的宽度
+     */
+    fun screenWidth(context: Context): Int {
+        return context.resources.displayMetrics.widthPixels
+    }
+
+    /**
+     * 获取控件的宽度
+     *
+     * @param view 要获取宽度的控件
+     * @return 控件的宽度
+     */
+    fun viewWidth(view: View): Int {
+        val w = View.MeasureSpec.makeMeasureSpec(
+            0,
+            View.MeasureSpec.UNSPECIFIED
+        )
+        val h = View.MeasureSpec.makeMeasureSpec(
+            0,
+            View.MeasureSpec.UNSPECIFIED
+        )
+        view.measure(w, h)
+        return view.measuredWidth
+    }
+
+    /**
+     * 获取控件的高度
+     *
+     * @param view 要获取高度的控件
+     * @return 控件的高度
+     */
+    fun viewHeight(view: View): Int {
+        val w = View.MeasureSpec.makeMeasureSpec(
+            0,
+            View.MeasureSpec.UNSPECIFIED
+        )
+        val h = View.MeasureSpec.makeMeasureSpec(
+            0,
+            View.MeasureSpec.UNSPECIFIED
+        )
+        view.measure(w, h)
+        return view.measuredHeight
+    }
+
+    /**
+     * 获取控件在窗体中的位置
+     *
+     * @param view 要获取位置的控件
+     * @return 控件位置的数组
+     */
+    fun getViewLocationInWindow(view: View): IntArray {
+        val location = IntArray(2)
+        view.getLocationInWindow(location)
+        return location
+    }
+
+    /**
+     * 获取控件在整个屏幕中的 位置
+     *
+     * @param view 要获取位置的控件
+     * @return 控件位置的数组
+     */
+    fun getViewLocationInScreen(view: View): IntArray {
+        val location = IntArray(2)
+        view.getLocationOnScreen(location)
+        return location
+    }
+
+    /**
+     * 获取设备的尺寸
+     */
+    fun getDeviceSize(context: Context): Double {
+        val dm = context.resources.displayMetrics
+        val sqrt = Math.sqrt(
+            Math.pow(
+                dm.widthPixels.toDouble(),
+                2.0
+            ) + Math.pow(dm.heightPixels.toDouble(), 2.0)
+        )
+        return sqrt / (160 * dm.density)
+    }
+
+    fun getDisplayMetrics(context: Context?): DisplayMetrics {
+        requireNotNull(context) { "context can't null" }
+        val windowManager =
+            context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val display = windowManager.defaultDisplay
+        val displayMetrics = DisplayMetrics()
+        display.getMetrics(displayMetrics)
+        return displayMetrics
+    }
+
+    fun getWindowWidth(context: Context?): Int {
+        val metrics = getDisplayMetrics(context)
+        return metrics.widthPixels
+    }
+
+    fun getWindowHeight(context: Context?): Int {
+        val metrics = getDisplayMetrics(context)
+        return metrics.heightPixels
+    }
+
+    fun getStatusBarHeight(context: Context): Int {
+        val resources = context.resources
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            val height = resources.getDimensionPixelSize(resourceId)
+            Log.v("dbw", "Status height:$height")
+            return height
+        }
+        return 0
+    }
+
+    fun getToolBarHeight(context: Context): Int {
+        val tv = TypedValue()
+        return if (context.theme.resolveAttribute(R.attr.actionBarSize, tv, true)) {
+            TypedValue.complexToDimensionPixelSize(
+                tv.data,
+                context.resources.displayMetrics
+            )
+        } else 0
+    }
+
+    fun getNavBarHeight(c: Context): Int {
+        var result = 0
+        val resources = c.resources
+        val orientation = c.resources.configuration.orientation
+        val resourceId: Int
+        resourceId = if (isTablet(c)) {
+            resources.getIdentifier(
+                if (orientation == Configuration.ORIENTATION_PORTRAIT) "navigation_bar_height" else "navigation_bar_height_landscape",
+                "dimen",
+                "android"
+            )
+        } else {
+            resources.getIdentifier(
+                if (orientation == Configuration.ORIENTATION_PORTRAIT) "navigation_bar_height" else "navigation_bar_width",
+                "dimen", "android"
+            )
+        }
+        if (resourceId > 0) {
+            result = c.resources.getDimensionPixelSize(resourceId)
+        }
+        Log.v("dbw", "Navi result:$result")
+        return result
+    }
+
+    private fun isTablet(c: Context): Boolean {
+        return ((c.resources.configuration.screenLayout
+                and Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE)
+    }
+
+    /**
+     * 获取虚拟按键的高度
+     */
+    fun getNavigationBarHeight(context: Context): Int {
+        var result = 0
+        if (hasNavBar(context)) {
+            val res = context.resources
+            val resourceId = res.getIdentifier("navigation_bar_height", "dimen", "android")
+            if (resourceId > 0) {
+                result = res.getDimensionPixelSize(resourceId)
+            }
+        }
+        return result
+    }
+
+    /**
+     * 检查是否存在虚拟按键栏
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    fun hasNavBar(context: Context): Boolean {
+        val res = context.resources
+        val resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android")
+        return if (resourceId != 0) {
+            var hasNav = res.getBoolean(resourceId)
+            // check override flag
+            val sNavBarOverride = navBarOverride
+            if ("1" == sNavBarOverride) {
+                hasNav = false
+            } else if ("0" == sNavBarOverride) {
+                hasNav = true
+            }
+            hasNav
+        } else { // fallback
+            !ViewConfiguration.get(context).hasPermanentMenuKey()
+        }
+    }
+
+    /**
+     * 判断虚拟按键栏是否重写
+     */
+    private val navBarOverride: String?
+        private get() {
+            var sNavBarOverride: String? = null
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                try {
+                    val c =
+                        Class.forName("android.os.SystemProperties")
+                    val m =
+                        c.getDeclaredMethod("get", String::class.java)
+                    m.isAccessible = true
+                    sNavBarOverride = m.invoke(null, "qemu.hw.mainkeys") as String
+                } catch (e: Throwable) {
+                }
+            }
+            return sNavBarOverride
+        }
+
+    fun getSoftKeyBoardHeight(context: Context): Int {
+        val height = screenHeight(context)
+        return height * 2 / 5
+    }
+}
